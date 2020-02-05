c5a87c99369771d87011009a0a2068f791b72953
Jonathanporter442 <60514488+Jonathanporter442@users.noreply.github.com>
2020-02-05 12:01:29 -0300

    Add files via upload
    clase 1

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    // asigno una variable
+    var nombre
+    // asigno el significado a la variable   
+    nombre = prompt ("ingrese su nombre");
+    // creo alerta con la variable
+    alert (nombre);
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,14 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+ var nombre;
+ // Se pide el archivo al html refiriendonos a el como "document" ponemos el punto y para pedir el dato en la caja de texto marcamos getElementByid y le marcamos la id del archivo a extraer("").value
+
+
+ 
+ nombre = document.getElementById("elNombre").value;
+// luego activamos una alerta para enviar el dato ingresado en la caja de texto
+ alert(nombre)
 }
 
 
