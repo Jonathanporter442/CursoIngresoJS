b0cb8b75f0f1851f8f3e45954aef0b17002e8a11
Jonathanporter442 <60514488+Jonathanporter442@users.noreply.github.com>
2020-02-06 15:44:34 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var random;
+	random = Math.round(Math.random()*10);
 	
+	if (random>=9)
+	{
+		alert("El resultado de tu examen es un excelente :" +random );
+	}
+	 else if (random>=4 && random<=8)
+	 {
+		 alert("Aprobo el resultado de tu examen es " +random);
+	 }
+	 else if (random<=3)
+	 {
+		 alert("Desaprobo el resultado de tu examen es : " +random);
+	 }
 
 }//FIN DE LA FUNCIÓN
