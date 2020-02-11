98b202131de0d26f7ddaed9bf3245448d8c4a3f5
Jonathanporter442 <60514488+Jonathanporter442@users.noreply.github.com>
2020-02-11 20:39:21 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,64 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño 
+
+mesDelAño= document.getElementById('mes').value;
+switch(mesDelAño)
+{
+
+case mesDelAño="Enero" : 
+{
+    alert("Que comeinces el año con todas las pilas");
+    break;
+}
+case mesDelAño="Marzo":
+{
+    alert("Empiezan las clases");
+    break;
+}
+case mesDelAño="Julio":
+    {
+        alert("Se vienen las vacaciones");
+        break;
+    }
+    case mesDelAño=("Diciembre"):
+    {
+    alert("¡Felices fiestas wachin!");
+    break;
+    }
 
 
 
 
-}//FIN DE LA FUNCIÓN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+}//FIN DE LA FUNCIÓN;
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,56 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+    case mesDelAño="Enero":
+    case mesDelAño="Febrero":
+    case mesDelAño="Marzo":
+    case mesDelAño="Abril":
+    case mesDelAño="Mayo":
+    case mesDelAño="Junio":
+        {
+            alert("Todavia no es invierno, lo que quiere decir que no eres feliz.");
+            break;
+
+        }
+    
+        case mesDelAño="Julio":
+        case mesDelAño="Agosto":
+
+{
+    alert("ES INVIERNO YA ERES FELIZ");
+    break;
+}
+case mesDelAño="Septiembre":
+case mesDelAño="Octubre":
+case mesDelAño="Noviembre":
+case mesDelAño="Diciembre":
+    {
+        alert("Ya paso el frio ahora te toca re cagarte de calor");
+        break;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
