fb15ac0f60a96bdf94247b593accf41fece59cbe
Maru-nya <jonathanporter442>
2020-02-15 21:23:05 -0300

    numero 8 resulto

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,8 +3,21 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta="si";
+	var suma;
+while (respuesta!="no")
+{
+suma = prompt("ingrese un numero");
+respuesta = prompt("ingrese otro numero");
 
+contador = parseInt(contador);
+acumulador = parseInt(acumulador);
+suma = parseInt(suma);
+
+contador = contador + 1;
+acumulador = suma + acumulador;
+
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,24 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
 	var respuesta='si';
+	var numero=0;
 
+	while (respuesta=="si")
+	{
+	numero = prompt("elija un numero");
+	numero = parseInt(numero);
+	if(numero>0){
+		positivo = positivo + numero;
+	}
+	 else if (numero<0)
+	 
+	{
+      negativo = negativo * numero;
+	 }
+	respuesta=prompt("si quiere continuar escriba SI, si usted no quiere continuar entonces coloque otra cosa");
+	contador=contador+1
+	}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
