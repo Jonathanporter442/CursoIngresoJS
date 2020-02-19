421e0c7a63dbaf3a84ca53305475defbc8fddaeb
Maru-nya <jonathanporter442>
2020-02-19 19:20:04 -0300

    ejercicio 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,100 +1,92 @@
 function mostrar()
 {
 
-	var contador=0;
+	
 	//declarar contadores y variables 
 	
-	var respuesta="si";
-	var negativos=0;
-	var cantidadNegativos=0;
-	var positivos=0;
-	var cantidadPositivos=0;
-	var ceros;
-	var numerospares;
-	var promedio;
-	var diferencia;
-	var sumadeparesparalospositivos;
-	var sumadeparesparalosnegativos;
-	var numerosparesparalospositivos;
-	var numerosparesparalosnegativos;
+	var respuesta="si" ;
+	var cantPositivos = 0 ;
+	var cantNegativos = 0 ;
+	var negativo = 0 ;
+	var positivo = 0 ;
+	var numero = 0 ;
+	var prompositivo = 0 ;
+	var promnegativo = 0 ;
+	var pardeposi = 0 ;
+	var pardenega = 0 ;
+	var resto = 0 ; 
+	var cero = 0 ;
+	var diferencia ;
+	var Parestotales ;
+
+	
 	while(respuesta!="no")
 	{
 		
-	numero = prompt("elija un numero");
-	numero = parseInt(numero);
-	if(numero>0){
-		
-		positivos = positivos + numero;
-	 
-	   cantidadPositivos = cantidadPositivos + 1;
-	 
-	   numerosparesparalospositivos = positivos % 2 ;
-	   
-	   promedio= positivos / numero;
-	   
-	   if(numerosparesparalopositivos==0)
-	  
-	 
-	  {
-	 
-		sumadeparesparalospositivos= sumadeparesparalospositivos+ 1;
-	  
+	numero = prompt("elija un numero") ;
+	numero = parseInt(numero) ;
 	
-	  }
-	   
-	   
-	}
-
-	 else if (numero<0)
-	 
+	if (numero>0)
 	{
-	 
-	
-		negativos = negativos + numero;
-	 
-	  cantidadNegativos = cantidadNegativos + 1;
-	 
-	  numerosparesparalosnegativos = negativos % 2;
-	 
-	  promedio = negativos / numero;
-	  
-	  
-	    if(numerosparesparalosnegativos==0)
-	  
-	 
-	  
+  
+		positivo = positivo + numero ;
+
+		cantPositivos = cantPositivos + 1 ;
+
+		resto = numero % 2 ;
+
+		if (resto==0)
 		{
-	 
-		sumadeparesparalosnegativos= sumadeparesparalosnegativos + 1;
-	  
-	
-	   }
-	  
-	  
-
-	}
-	 
-	else if(numero==0)
-	
-	    {
-		 ceros = ceros +1;
-	
+			pardeposi = pardeposi + 1 ;
 		}
-	
-	 
-    diferencia = positivos - negativos; 
-	 
 
-	respuesta=prompt("si quiere continuar escriba si, si usted no quiere continuar entonces coloque no");
+		else if (numero<0)
+		{
+          negativo = negativo + numero ;
+		 
+		  cantNegativos = cantNegativos + 1 ;
+		  
+		  resto = numero % 2 ;
+
+		  if (resto==0)
+		  {
+			  pardenega = pardenega + 1 ;
+		  }
+		}
+
+		else
+		{
+			cero = cero + 1 ;
+		}
+
+	 respuesta = prompt("Si ya no quiere continuar ingresando numero entonces escriba no en caso contrario escriba si");
+
+
 	}
-	document.write("La suma de los positivos es : "+ positivos );
-	document.write("La suma de los negativos es : "+ negativos );
-	document.write("La cantidad de positivos es : "+ cantidadPositivos);
-	document.write("La cantidad de negativos es : "+ cantidadNegativos );
-	document.write("La cantidad de numeros pares es : "+ numerosparesparalospositivos );
-	document.write("La cantidad de numeros pares es : "+ numerosparesparalosnegativos);
-	document.write("El promedio es :"+ promedio );
-	document.write("La diferencia de los numeros positivos y negativos es :"+ diferencia);
+
+}
+
+
+diferencia = positivo - negativo ;
+
+prompositivo = positivo / cantPositivos ;
+
+promnegativo = negativo / cantNegativos ;
+
+Parestotales = positivo + negativo ;
+		
+
+document.write("La suma de los numeros positivos es : " + positivo);
+document.write("La suma de los numeros negativos es : " + negativo);
+document.write("La cantidad de numeros positivos es : " + cantPositivos);
+document.write("La cantidad de numeros negativos es : " + cantNegativos);
+document.write("La cantidad de ceros es :" + ceros);
+document.write("El promedio de los numeros positivos es : " + prompositivo);
+document.write("El promedio de los numeros negativos es : " + promnegativo);
+document.write("La diferencia entre los numeros positivos y los negativos es : " + diferencia);
+document.write("La cantidad de numeros pares es : " + Parestotales);
+
+	
 	
 	
 
